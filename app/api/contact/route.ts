349bf76757ba6dffee284977d2e7c7bf49cba1d8
RND <rnd@Macbook-Pro.local>
2024-01-27 16:26:14 +0530

    ENV variable replaced

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-const SEND_EMAIL = 'contact.simpledevcode@gmail.com';
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   auth: {
-    user: SEND_EMAIL,
-    pass: "gyuy nqig mhdg pkth",
+    user: process.env.SEND_EMAIL,
+    pass: process.env.PASS,
   },
 });
 
@@ -16,7 +15,7 @@ export async function POST(request: Request) {
   const data = await request.json();
   const info = await transporter.sendMail({
     from: data.email, // sender address
-    to: SEND_EMAIL, // list of receivers
+    to: process.env.SEND_EMAIL, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: `<b>Name: ${data?.fullName}</b><br/><b>Name: ${data?.email}</b><br/><b>Budget: ${data?.budget}</b><br/><b>Description: ${data?.description}</b><b>Contact No: ${data?.number}</b>`, // html body
